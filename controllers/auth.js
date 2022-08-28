991c19f0398e72efbc1ce6ee9b331f8a11eaf958
Timilehin Arigbede <Harriemannie@gmail.com>
2022-08-28 14:44:08 +0100

    Add Validation to login

--- controllers/auth.js
+++ controllers/auth.js
@@ -55,11 +55,20 @@ exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
 
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.render("auth/login", {
+      path: "/login",
+      pageTitle: "Login",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
         req.flash("error", "Invalid email or password.");
-        res.redirect("/login");
+        return res.redirect("/login");
       }
       bcrypt
         .compare(password, user.password)
